d941f24f107d20ab6c70fbc1b586bce937c9483b
Jay arre P. Talosig <35078433+flexycode@users.noreply.github.com>
2018-11-12 03:39:34 +0800

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,32 @@
+/* ============================================================
+   Sidenav
+ * ============================================================*/
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+$(document).on('click', function (e) {
+    if ($(e.target).closest("#mySidenav, .slide-menu").length === 0) {
+       document.getElementById("mySidenav").style.width = "0";
+    }
+});
+// javascript Animate onscroll Start
+    $(document).ready(function () {
+    if (screen.width > 1024) {
+    AOS.init({
+    easing: 'ease-in-out-sine',
+    once: true,
+    });
+    }
+    });
+//  Testimmonials
+$(document).ready(function() {
+  //carousel options
+  $('#quote-carousel').carousel({
+    pause: true, interval: 3000,
+  });
+});
