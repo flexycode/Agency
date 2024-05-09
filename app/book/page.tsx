4f866ce01c3fccfda066c4cb3d3ca2327f89022c
Jay Arre P. Talosig <35078433+flexycode@users.noreply.github.com>
2024-05-09 10:40:01 +0000

    💫 Add files

---
+++ app/book/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { InlineWidget } from "react-calendly";
+
+import Navbar from "@/components/navbar";
+
+const Book = () => {
+  return (
+    <>
+      <div className="flex flex-col   w-full  h-screen bg-black/[0.96]  bg-grid-white/[0.02]   ">
+        <Navbar
+          scrollToWebsiteDesign={() => {}}
+          scrollToGraphicDesign={() => {}}
+          scrollToShopifyStores={() => {}}
+          scrollToBrands={() => {}}
+          scrollToServices={() => {}}
+        />
+        <div className="text-4xl pb-5 md:text-6xl text-center 
+        bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+          Book a meeting
+        </div>
+
+        <InlineWidget url="https://calendly.com/flexyledger/30min" />
+      </div>
+    </>
+  );
+};
+
+export default Book;
